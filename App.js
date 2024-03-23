91343ff5d20e2cc0a2afe52a235a5df8a0536daf
Hadestech01 <olaniyijohnayomide@gmail.com>
2024-03-23 21:24:48 +0100

    Add linting and additional dependencies

--- App.js
+++ App.js
@@ -1,38 +1,13 @@
-import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import {
-  StatusBar,
-  StyleSheet,
-  SafeAreaView,
-  Platform,
-  Text,
-  View,
-} from "react-native";
+import React from "react";
 
-// const isAndroid = Platform.OS === "android";
-
-console.log(StatusBar.currentHeight);
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={{ padding: 16, backgroundColor: "green" }}>
-          <Text>Search</Text>
-        </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-    // paddingTop: 40,
-  },
-});
